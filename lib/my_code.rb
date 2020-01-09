3ba711c1ce1938e74dc66a89b0739cb419507f35
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-09 02:32:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(array)
+  new = []
+  index = 0 
+  while index < array.length do
+    new << yield(array[index])
+    index += 1 
+  end
+  new
+end
+
+def reduce(array, sp = nil)
+  if sp
+    sum = sp
+    index = 0
+  else
+    sum = array[0]
+    index = 1
+  end
+
+  while index < array.length do
+    sum = yield(sum, array[index])
+    index += 1 
+  end
+  sum
+end
