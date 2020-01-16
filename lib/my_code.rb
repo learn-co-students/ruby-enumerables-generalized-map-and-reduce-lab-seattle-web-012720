6b68915f3e0d6b0903d7d737d11833d69d544a88
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-16 06:42:29 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(array)
+ new=[]
+  i=0
+while i<array.length
+new.push(yield(array[i]))
+i+=1
+end
+new
+end
+
+def reduce(array,sp=nil)
+  if sp
+    sum=sp
+    i=0
+  else 
+    sum=array[0]
+    i=1
+
+end
+while i<array.length
+sum = yield(sum, array[i])
+i=+1 
+end
+sum
+end
